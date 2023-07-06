d00f128f2525ec09226b693464442764fb1c71b7
Hristo Koev <iamhristokoev@gmail.com>
2023-07-06 16:09:08 +0200

    init

---
+++ src/app/projects/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+	title: 'Projects',
+	description: 'Projects',
+}
+
+export default function Projects() {
+	return (
+	  <main className="">
+		Hey
+	  </main>
+	)
+  }
+  
